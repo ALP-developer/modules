7f49c5f8d77fe1555c2b2559305a2e6757413c0a
Alperen <92602100+ALP-developer@users.noreply.github.com>
2021-10-18 20:06:38 +0300

    Add files via upload

---
+++ modul1.py
@@ -0,0 +1,16 @@
+programlama_dilleri = ["Pyhton", "Phy", "Java", "C"]
+
+
+def toplama(a,b,c):
+    """
+    Bu fonsiyon 3 tane değeri döndürür.
+
+    """
+    return a + b + c
+def selamla(isim):
+    """
+
+    Bu fonsiyon İnsanları selamlar.
+
+    """
+    print("Selam",isim)
